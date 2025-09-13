46e4b6d76a3b6eec2e389785770cb88a7be4147f
micr0softstore <balazs.kokai.builds@gmail.com>
2025-09-13 20:59:12 +0200

    bpf: remove 'free_map_nouncharge' as its unused

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -283,9 +283,6 @@ free_map:
 	bpf_map_uncharge_memlock(map);
 free_map_sec:
 	security_bpf_map_free(map);
-free_map_nouncharge:
-	map->ops->map_free(map);
-	return err;
 }
 
 /* if error is returned, fd is released.
